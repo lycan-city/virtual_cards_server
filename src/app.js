2d64b97cc2e22dd77db9910688ede21208aaf4af
Maximo Dominguez <mamodom@gmail.com>
2017-02-18 04:33:59 -0400

    Once again adding naive implementations Implemented `/start`

--- src/app.js
+++ src/app.js
@@ -7,6 +7,28 @@ const storage = {
   games: {}
 };
 
+const fakeGame = {
+  create: players => [...Array(players).keys()]
+};
+
+function shuffle(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -27,8 +49,23 @@ app.post('/host', (req, res) => {
   });
 });
 
+app.post('/start', (req, res) => {
+  const gameId = req.body.gameId;
+  if (!gameId)
+    return res.status(400).json({ error: 'gameId cannot be undefined' });
+
+  if (!storage.games[gameId])
+    return res.status(400).json({ error: `no game with id ${gameId} found` });
+
+  const game = shuffle(fakeGame.create(storage.games[gameId].players.length));
+
+  storage.games[gameId].players.forEach((p, i) => p.card = game[i]);
+
+  res.json(storage.games[gameId]);
+});
+
 app.post('/join', (req, res) => {
-  if(!req.body.gameId)
+  if (!req.body.gameId)
     return res.status(400).json({ error: 'gameId cannot be undefined' });
 
   if (!storage.games[req.body.gameId])
@@ -36,7 +73,7 @@ app.post('/join', (req, res) => {
 
   const userId = req.body.userId || uuid.v4();
 
-  storage.games[req.body.gameId].players.push(userId);
+  storage.games[req.body.gameId].players.push({ id: userId });
 
   res.json({
     userId: userId,
