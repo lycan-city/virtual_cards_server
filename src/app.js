300991ffd6e1f69b880b9fc05a59b908c165db6b
Maximo Dominguez <mamodom@gmail.com>
2017-02-18 03:40:41 -0400

    Add a basic implementation of 'host game' and 'join game'

--- src/app.js
+++ src/app.js
@@ -1,12 +1,49 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import uuid from 'node-uuid';
 
 const app = express();
+const storage = {
+  games: {}
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>
     res.send('hello world')
 );
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-  console.log('http://localhost:3000');
-})
+app.post('/host', (req, res) => {
+  const userId = req.body.userId || uuid.v4();
+  const gameId = uuid.v4();
+  storage.games[gameId] = {
+    host: userId,
+    players: []
+  };
+  res.json({
+    gameId: gameId,
+    userId: userId
+  });
+});
+
+app.post('/join', (req, res) => {
+  if(!req.body.gameId)
+    return res.status(400).json({ error: 'gameId cannot be undefined' });
+
+  if (!storage.games[req.body.gameId])
+    return res.status(400).json({ error: `no game with id ${req.body.gameId} found` });
+
+  const userId = req.body.userId || uuid.v4();
+
+  storage.games[req.body.gameId].players.push(userId);
+
+  res.json({
+    userId: userId,
+    game: storage.games[req.body.gameId]
+  });
+});
+
+app.listen(3000);
+
+console.log('http://localhost:3000');
