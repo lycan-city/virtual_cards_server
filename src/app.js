0890e39da3768a5d736459a2218b0ba63b2e99bd
Maximo Dominguez <mamodom@gmail.com>
2017-04-08 23:27:57 -0400

    Returning the entire party object in :host

--- src/app.js
+++ src/app.js
@@ -7,52 +7,53 @@ const app = express();
 app.set('port', (process.env.PORT || 3000));
 
 const storage = {
-  parties: {}
+    parties: {}
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) =>
-  res.json(storage)
+    res.json(storage)
 );
 
 app.post('/host', (req, res) => {
-  if(!req.body.userId)
-    return res.status(400)
-              .json({ error: 'userId cannot be undefined' });
+    if(!req.body.userId)
+        return res.status(400)
+            .json({ error: 'userId cannot be undefined' });
 
-  const partyId = uuid.v4().substr(0,4);
+    const partyId = uuid.v4().substr(0,4);
 
-  storage.parties[partyId] = {
-    players: [{
-      id: req.body.userId,
-      name: req.body.userId,
-      host: true
-    }]
-  };
+    const party = {
+        partyId: partyId,
+        players: [{
+            id: req.body.userId,
+            name: req.body.userId,
+            host: true
+        }]
+    };
 
-  res.json({
-    partyId: partyId
-  });
+    storage.parties[partyId] = party;
+
+    res.json(party);
 });
 
 app.post('/join', (req, res) => {
-  if (!req.body.partyId)
-    return res.status(400).json({ error: 'partyId cannot be undefined' });
+    if (!req.body.partyId)
+        return res.status(400).json({ error: 'partyId cannot be undefined' });
 
-  if(!req.body.userId)
-    return res.status(400).json({ error: 'userId cannot be undefined' });
+    if(!req.body.userId)
+        return res.status(400).json({ error: 'userId cannot be undefined' });
 
-  if (!storage.parties[req.body.partyId])
-    return res.status(404).json({ error: `no party with id ${req.body.partyId} found` });
+    if (!storage.parties[req.body.partyId])
+        return res.status(404).json({ error: `no party with id ${req.body.partyId} found` });
 
-  storage.parties[req.body.partyId].players.push({
-    id: req.body.userId,
-    name: req.body.userId,
-  });
+    storage.parties[req.body.partyId].players.push({
+        id: req.body.userId,
+        name: req.body.userId,
+    });
 
-  res.json(storage.parties[req.body.partyId]);
+    res.json(storage.parties[req.body.partyId]);
 });
 
 app.listen(app.get('port'));
