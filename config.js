f779484c7d817fe7d18e68d67d7c45b968c5393e
Willy Ovalle <willyovalle16@gmail.com>
2017-04-10 23:39:39 -0400

    Fix: removing unused variables

--- config.js
+++ config.js
@@ -2,4 +2,3 @@ exports.port = process.env.NODE_PORT || 3000;
 exports.pusherAppId = process.env.PUSHER_APP_ID || '';
 exports.pusherKey = process.env.PUSHER_KEY || '';
 exports.pusherSecret = process.env.PUSHER_SECRET || '';
-exports.pusherEncrypted = process.env.PUSHER_ENCRYPTED || true;
